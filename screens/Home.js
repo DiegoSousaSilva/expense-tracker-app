a30bc644ee599ebb486d8f0c660cdb01fb5c3be9
Diego Sousa <derickbenji2@gmail.com>
2021-01-24 00:53:46 -0300

    criando omponents | usando hooks | UX/UI

--- screens/Home.js
+++ screens/Home.js
@@ -1,9 +1,225 @@
-import React from 'react';
-import {View, Text, TouchableOpacity, Image} from 'react-native';
+import React, {useState, useRef} from 'react';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+  ScrollView,
+  FlatList,
+  StyleSheet,
+  Animated,
+} from 'react-native';
 
 import {COLORS, FONTS, SIZES, icons} from '../constants';
 
 const Home = () => {
+  // Dummy Date
+  const confirmStatus = 'C';
+  const pendingStatus = 'P';
+
+  let categoriesData = [
+    {
+      id: 1,
+      name: 'Education',
+      icon: icons.education,
+      color: COLORS.yellow,
+      expenses: [
+        {
+          id: 1,
+          title: 'Tution Fee',
+          description: 'Tuition fee',
+          location: "ByProgramers' tution center",
+          total: 100.0,
+          status: pendingStatus,
+        },
+        {
+          id: 2,
+          title: 'Arduino',
+          description: 'Hardward',
+          location: "ByProgramers' tution center",
+          total: 30.0,
+          status: pendingStatus,
+        },
+        {
+          id: 3,
+          title: 'Javascript Books',
+          description: 'Javascript books',
+          location: "ByProgramers' Book Store",
+          total: 20.0,
+          status: confirmStatus,
+        },
+        {
+          id: 4,
+          title: 'PHP Books',
+          description: 'PHP books',
+          location: "ByProgramers' Book Store",
+          total: 20.0,
+          status: confirmStatus,
+        },
+      ],
+    },
+    {
+      id: 2,
+      name: 'Nutrition',
+      icon: icons.food,
+      color: COLORS.lightBlue,
+      expenses: [
+        {
+          id: 5,
+          title: 'Vitamins',
+          description: 'Vitamin',
+          location: "ByProgramers' Pharmacy",
+          total: 25.0,
+          status: pendingStatus,
+        },
+        {
+          id: 6,
+          title: 'Protein powder',
+          description: 'Protein',
+          location: "ByProgramers' Pharmacy",
+          total: 50.0,
+          status: confirmStatus,
+        },
+      ],
+    },
+    {
+      id: 3,
+      name: 'Child',
+      icon: icons.baby_car,
+      color: COLORS.darkgreen,
+      expenses: [
+        {
+          id: 7,
+          title: 'Toys',
+          description: 'toys',
+          location: "ByProgramers' Toy Store",
+          total: 25.0,
+          status: confirmStatus,
+        },
+        {
+          id: 8,
+          title: 'Baby Car Seat',
+          description: 'baby car seat',
+          location: "ByProgramers' Baby Care Store",
+          total: 100.0,
+          status: pendingStatus,
+        },
+        {
+          id: 9,
+          title: 'Pampers',
+          description: 'pampers',
+          location: "ByProgramers'  Supermarket",
+          total: 100.0,
+          status: pendingStatus,
+        },
+        {
+          id: 10,
+          title: 'Baby T-Shirt',
+          description: 't-shirt',
+          location: "ByProgramers' Fashion Store",
+          total: 20.0,
+          status: pendingStatus,
+        },
+      ],
+    },
+    {
+      id: 4,
+      name: 'Beauty & Care',
+      icon: icons.healthcare,
+      color: COLORS.peach,
+      expenses: [
+        {
+          id: 11,
+          title: 'Skin Care product',
+          description: 'skin care',
+          location: "ByProgramers' Pharmacy",
+          total: 10.0,
+          status: pendingStatus,
+        },
+        {
+          id: 12,
+          title: 'Lotion',
+          description: 'lotion',
+          location: "ByProgramers' Pharmacy",
+          total: 50.0,
+          status: confirmStatus,
+        },
+        {
+          id: 13,
+          title: 'Face Mask',
+          description: 'face mask',
+          location: "ByProgramers' Pharmacy",
+          total: 50.0,
+          status: pendingStatus,
+        },
+        {
+          id: 14,
+          title: 'Sunscreen cream',
+          description: 'sunscreen cream',
+          location: "ByProgramers' Pharmacy",
+          total: 50.0,
+          status: pendingStatus,
+        },
+      ],
+    },
+    {
+      id: 5,
+      name: 'Sports',
+      icon: icons.sports_icon,
+      color: COLORS.purple,
+      expenses: [
+        {
+          id: 15,
+          title: 'Gym Membership',
+          description: 'monthly fee',
+          location: "ByProgramers' Gym",
+          total: 45.0,
+          status: pendingStatus,
+        },
+        {
+          id: 16,
+          title: 'Gloves',
+          description: 'Gym Eequipment',
+          location: "ByProgramers' Gym",
+          total: 15.0,
+          status: confirmStatus,
+        },
+      ],
+    },
+    {
+      id: 6,
+      name: 'Clothing',
+      icon: icons.cloth_icon,
+      color: COLORS.red,
+      expenses: [
+        {
+          id: 17,
+          title: 'T-Shirt',
+          description: 'Plain Color T-Shirt',
+          location: "ByProgramers' Mall",
+          total: 20.0,
+          status: pendingStatus,
+        },
+        {
+          id: 18,
+          title: 'Jeans',
+          description: 'blue jeans',
+          location: "ByProgramers' Mall",
+          total: 50.0,
+          status: confirmStatus,
+        },
+      ],
+    },
+  ];
+
+  const categoryListHeightAnimationValue = useRef(new Animated.Value(115))
+    .current;
+
+  const [categories, setCategories] = useState(categoriesData);
+  const [viewMode, setViewMode] = useState('chart');
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [showMoreToogle, setShowMoreToogle] = useState(false);
+
   function renderNavBar() {
     return (
       <View
@@ -44,11 +260,237 @@ const Home = () => {
     );
   }
 
+  function renderHeader() {
+    return (
+      <View
+        style={{
+          paddingHorizontal: SIZES.padding,
+          paddingVertical: SIZES.padding,
+          backgroundColor: COLORS.white,
+        }}>
+        <View>
+          <Text style={{color: COLORS.primary, ...FONTS.h2}}>My Expenses</Text>
+          <Text style={{color: COLORS.darkgray, ...FONTS.h3}}>
+            Sumary (private)
+          </Text>
+        </View>
+
+        <View
+          style={{
+            flexDirection: 'row',
+            marginTop: SIZES.padding,
+            alignItems: 'center',
+          }}>
+          <View
+            style={{
+              height: 50,
+              width: 50,
+              backgroundColor: COLORS.lightGray,
+              borderRadius: 25,
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+            <Image
+              source={icons.calendar}
+              style={{
+                width: 20,
+                height: 20,
+                tintColor: COLORS.lightBlue,
+              }}
+            />
+          </View>
+          <View style={{marginLeft: SIZES.padding}}>
+            <Text style={{color: COLORS.primary, ...FONTS.h3}}>
+              23 Jan, 2021
+            </Text>
+            <Text style={{color: COLORS.darkgray, ...FONTS.body3}}>
+              18% more than last month
+            </Text>
+          </View>
+        </View>
+      </View>
+    );
+  }
+
+  function renderCategoryHeaderSection() {
+    return (
+      <View
+        style={{
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+          padding: SIZES.padding,
+        }}>
+        {/* Title */}
+        <View>
+          <Text style={{color: COLORS.primary, ...FONTS.h3}}>Category</Text>
+          <Text style={{color: COLORS.darkgray, ...FONTS.body4}}>
+            {categories.length} Total
+          </Text>
+        </View>
+
+        {/* Buttons */}
+        <View style={{flexDirection: 'row'}}>
+          <TouchableOpacity
+            style={{
+              alignItems: 'center',
+              justifyContent: 'center',
+              height: 50,
+              width: 50,
+              backgroundColor: viewMode == 'chart' ? COLORS.secondary : null,
+              borderRadius: 25,
+            }}
+            onPress={() => {
+              setViewMode('chart');
+            }}>
+            <Image
+              source={icons.chart}
+              resizeMode="contain"
+              style={{
+                width: 20,
+                height: 20,
+                tintColor: viewMode == 'chart' ? COLORS.white : COLORS.darkgray,
+              }}
+            />
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={{
+              alignItems: 'center',
+              justifyContent: 'center',
+              height: 50,
+              width: 50,
+              backgroundColor: viewMode == 'list' ? COLORS.secondary : null,
+              borderRadius: 25,
+            }}
+            onPress={() => {
+              setViewMode('list');
+            }}>
+            <Image
+              source={icons.menu}
+              resizeMode="contain"
+              style={{
+                width: 20,
+                height: 20,
+                tintColor: viewMode == 'list' ? COLORS.white : COLORS.darkgray,
+              }}
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
+
+  function renderCategoryList() {
+    const renderItem = ({item}) => {
+      return (
+        <TouchableOpacity
+          style={{
+            flex: 1,
+            flexDirection: 'row',
+            margin: 5,
+            paddingVertical: SIZES.radius,
+            paddingHorizontal: SIZES.padding,
+            borderRadius: 5,
+            backgroundColor: COLORS.white,
+            ...styles.shadow,
+          }}
+          onPress={() => setSelectedCategory(item)}>
+          <Image
+            source={item.icon}
+            style={{
+              width: 20,
+              height: 20,
+              tintColor: item.color,
+            }}
+          />
+          <Text
+            style={{
+              marginLeft: SIZES.base,
+              color: COLORS.primary,
+              ...FONTS.h4,
+            }}>
+            {item.name}
+          </Text>
+        </TouchableOpacity>
+      );
+    };
+
+    return (
+      <View style={{paddingHorizontal: SIZES.padding - 5}}>
+        <Animated.View style={{height: categoryListHeightAnimationValue}}>
+          <FlatList
+            data={categories}
+            renderItem={renderItem}
+            keyExtractor={(item) => `${item.id}`}
+            numColumns={2}
+          />
+        </Animated.View>
+
+        <TouchableOpacity
+          style={{
+            flexDirection: 'row',
+            marginVertical: SIZES.base,
+            justifyContent: 'center',
+          }}
+          onPress={() => {
+            if (showMoreToogle) {
+              Animated.timing(categoryListHeightAnimationValue, {
+                toValue: 115,
+                duration: 300,
+                useNativeDriver: false,
+              }).start();
+            } else {
+              Animated.timing(categoryListHeightAnimationValue, {
+                toValue: 172.5,
+                duration: 300,
+                useNativeDriver: false,
+              }).start();
+            }
+
+            setShowMoreToogle(!showMoreToogle);
+          }}>
+          <Text style={{...FONTS.body4}}>
+            {showMoreToogle ? 'LESS' : 'MORE'}
+          </Text>
+          <Image
+            source={showMoreToogle ? icons.up_arrow : icons.down_arrow}
+            style={{marginLeft: 5, width: 15, height: 15, alignSelf: 'center'}}
+          />
+        </TouchableOpacity>
+      </View>
+    );
+  }
+
   return (
     <View style={{flex: 1, backgroundColor: COLORS.lightGray2}}>
+      {/* Nav Bar Section */}
       {renderNavBar()}
+
+      {/* Header Section */}
+      {renderHeader()}
+
+      {/* Category Header Section */}
+      {renderCategoryHeaderSection()}
+
+      <ScrollView contentContainerStyle={{paddingBottom: 60}}>
+        {viewMode == 'list' && <View>{renderCategoryList()}</View>}
+      </ScrollView>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  shadow: {
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 2,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 3,
+  },
+});
 
 export default Home;
